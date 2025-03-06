86ffbb010a0748edca9bbb99582f6f48a6ab2cfa
Alejandro Paniagua <apaniaguu@alumnos.unex.es>
2025-03-06 18:25:21 +0100

    Componentes del carrito

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css'
-import { SignUpCard } from '@/components/cards/SignUpCard'
+import{PaginaCesta} from '@/routes/PaginaCesta'
 
 function App() {
   return (
-    <div className='flex items-center justify-center h-screen w-screen'>
-      <SignUpCard/>
-    </div>
+      <div className='overflow-auto w-screen min-h-screen h-max bg-gradient-to-br from-green-900 to-blue-800'>   
+         <PaginaCesta></PaginaCesta>
+      </div>
   )
 }
 
