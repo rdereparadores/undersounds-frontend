2cd4664e4345ac543c2a4dce67902d385adfe4fb
Iván Ruiz López <ivruizl@alumnos.unex.es>
2025-03-06 17:08:52 +0100

    Added LogInCard and SignUpCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css'
-import { SignUpCard } from '@/components/cards/SignUpCard'
 
 function App() {
   return (
     <div className='flex items-center justify-center h-screen w-screen'>
-      <SignUpCard/>
     </div>
   )
 }
