40b334fee84524b39949539541638c4679f4567c
Iván Ruiz López <ivruizl@alumnos.unex.es>
2025-03-07 10:12:20 +0100

    Default route is now Index

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { CartProvider } from './hooks/cartContext.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-import Shop from "@/routes/Shop.tsx"
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <CartProvider>
-            <Shop />
-        </CartProvider>
+        <App />
     </React.StrictMode>
 );
