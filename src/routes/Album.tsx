55407a52cb6a2fcca186f3681c91f50d483a3e57
Iván Ruiz López <ivruizl@alumnos.unex.es>
2025-03-23 12:22:05 +0100

    Fixed compilation issues

--- src/routes/Album.tsx
+++
@@ -1,7 +0,0 @@
-import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
-
-export const Album = () => {
-    return (
-        <></>
-    )
-}
